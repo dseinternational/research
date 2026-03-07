8ec699fe58b2088f22434754450f37c30873fb91
Frank Buckley <frank@frankbuckley.com>
2026-03-07 14:12:03 +0000

    Create distributions.py

---
+++ src/python/src/dse_research_utils/plot/distributions.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2026 Down Syndrome Education International and contributors
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import matplotlib.pyplot as plt
+import os
+
+from preliz.distributions.distributions import Continuous
+
+def plot_distribution(dist: Continuous, output_dir: str, filename: str):
+    dist.plot_pdf()
+    if output_dir is not None and filename is not None:
+        plt.savefig(os.path.join(output_dir, f"{filename}.png"), dpi=300)
+        plt.savefig(os.path.join(output_dir, f"{filename}.svg"))

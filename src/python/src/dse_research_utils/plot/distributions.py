9c4611c04a9de775bd7ddd424a12ff21a222bf5f
Frank Buckley <frank@frankbuckley.com>
2026-03-08 12:15:50 +0000

    Update distributions.py

--- src/python/src/dse_research_utils/plot/distributions.py
+++ src/python/src/dse_research_utils/plot/distributions.py
@@ -3,11 +3,13 @@
 
 import matplotlib.pyplot as plt
 import os
+from matplotlib.figure import Figure
 
 from preliz.distributions.distributions import Continuous
 
-def plot_distribution(dist: Continuous, output_dir: str, filename: str):
+def plot_distribution(dist: Continuous, output_dir: str, filename: str) -> Figure:
     dist.plot_pdf()
     if output_dir is not None and filename is not None:
         plt.savefig(os.path.join(output_dir, f"{filename}.png"), dpi=300)
         plt.savefig(os.path.join(output_dir, f"{filename}.svg"))
+    return plt.gcf()

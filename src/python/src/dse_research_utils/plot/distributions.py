0149078d938ceeea55563e9d981182c20085e0e1
Frank Buckley <frank@frankbuckley.com>
2026-03-12 15:37:20 +0000

    Updates

--- src/python/src/dse_research_utils/plot/distributions.py
+++ src/python/src/dse_research_utils/plot/distributions.py
@@ -4,11 +4,14 @@
 import os
 
 import matplotlib.pyplot as plt
+import dse_research_utils.plot.styles as plot_styles
+
 from matplotlib.figure import Figure
 from preliz.distributions.distributions import Continuous
 
 
 def plot_distribution(dist: Continuous, output_dir: str, filename: str) -> Figure:
+    plt.figure(figsize=plot_styles.FIGSIZE_MD)
     dist.plot_pdf()
     if output_dir is not None and filename is not None:
         plt.savefig(os.path.join(output_dir, f"{filename}.png"), dpi=300)
